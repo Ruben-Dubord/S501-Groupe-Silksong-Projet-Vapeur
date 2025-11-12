5e416677e5acb60a2145725ed07768934c39e723
Ruben-Dubord <rubendubord95@gmail.com>
2025-11-12 12:58:48 +0100

    Ajout des éléments de la page profile pour naviguer entre les vues plus tard

--- src/views/profileView.tsx
+++ src/views/profileView.tsx
@@ -1,48 +1,80 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { useState } from 'react';
+import { StyleSheet, Text, Image, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Card from '@/components/Card';
+import { Link } from 'expo-router';
 
 export default function App() {
-  const [count, setCount] = useState<number>(0);
+
+    const games = [
+    { id: 1, name: "Hollow Knight", image: "../assets/games/hollow-knight.png", liked: true },
+    { id: 2, name: "Elden Ring", image: "../assets/games/elden-ring.png", liked: true },
+    { id: 3, name: "Dark Souls", image: "../assets/games/dark-souls.png", liked: true },
+    { id: 4, name: "Celeste", image: "../assets/games/celeste.png", liked: false },
+    {
+        id: 5,
+        name: "Stardew Valley",
+        image: "../assets/games/stardew-valley.png",
+        liked: false,
+    },
+    ];
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Bienvenue sur mon app React Native (TS) !</Text>
+    <SafeAreaView>
+        <Text style={styles.title}>Welcome to your profile !</Text>
+        
+        <Card>
+            <Link href="/"/>
+            <View style={styles.rowContainer}>
+            <Image source={require('@/assets/images/favorites-icon.png')} style={styles.category} />
+            <View style= {styles.textColumn}>
+                <Text style={styles.categoryTitle}>Your favorite games</Text>
+                <Text>{games.filter((game) => game.liked).length} games liked</Text>
+            </View>
+            </View>
+        </Card>
 
-      <Text style={styles.counter}>Compteur : {count}</Text>
-
-      <TouchableOpacity style={styles.btn} onPress={() => setCount(prev => prev + 1)}>
-        <Text style={styles.btnText}>+1</Text>
-      </TouchableOpacity>
+        <Card>
+            <Link href="/"/> 
+            <View style={styles.rowContainer}>
+            <Image source={require('@/assets/images/settings-icon.svg')} style={styles.category} />
+            <View style= {styles.textColumn}>
+                <Text style={styles.categoryTitle}>Your settings</Text>
+                <Text>App preferences</Text>
+            </View>
+            </View>
+        </Card>
 
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fff',
-  },
   title: {
     fontSize: 24,
     fontWeight: '600',
     marginBottom: 20,
+    textAlign: 'center',
   },
-  counter: {
-    fontSize: 20,
-    marginVertical: 20,
+
+  rowContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 20,
   },
-  btn: {
-    backgroundColor: '#007AFF',
-    paddingVertical: 12,
-    paddingHorizontal: 25,
-    borderRadius: 10,
+
+  category: {
+    width: 100,
+    height: 100,
+    borderRadius: 8,
   },
-  btnText: {
-    color: '#fff',
-    fontSize: 18,
+
+  textColumn: {
+    marginLeft: 12,
+    justifyContent: 'center',
   },
-});
+
+  categoryTitle: {
+    fontWeight: '600',
+    marginBottom: 4,
+  },
+}); 
