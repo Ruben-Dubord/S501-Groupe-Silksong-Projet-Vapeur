02a25b5c7ac35a3c6ac5179eda508cf2e3993d85
Enrike Champion <enrikechampion@gmail.com>
2025-11-14 15:40:20 +0100

    Mise en place de la BD + ajout d'un nouveau script pour générer automatiquement index.ts

---
+++ src/app/exemple-BD.tsx
@@ -0,0 +1,64 @@
+import { ScrollView, Text, Image, View } from "react-native";
+import { SQLiteProvider, useSQLiteContext } from "expo-sqlite";
+import { useEffect, useState } from "react";
+import { gameImages } from "../assets/images/GameImages";
+
+type game = {
+  AppID: number;
+  Name: string;
+  RequiredAge: number;
+  Price: number;
+  Description: string;
+  HeaderImage: string;
+  Developers: string;
+  Publishers: string;
+  Tags: string;
+  Liked: boolean;
+};
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <SQLiteProvider databaseName="vapeur-1-1.db" assetSource={{ assetId: require("../database/vapeur.db") }}>
+        <Test />
+      </SQLiteProvider>
+    </View>
+  );
+}
+
+function getGameImage(appId: number) {
+  try {
+    return gameImages[appId];
+  } catch (e) {
+    return require("../assets/images/partial-react-logo.png");
+  }
+}
+
+export function Test() {
+  const db = useSQLiteContext();
+  const [test, setTest] = useState<game[]>([]);
+  useEffect(() => {
+    async function fetchData() {
+      const result = await db.getAllAsync<game>("SELECT * FROM games ORDER BY RANDOM();");
+      setTest(result);
+    }
+    fetchData();
+  }, []);
+  return (
+    <ScrollView>
+      {test.map((item) => (
+        <Image
+          key={item.AppID}
+          source={getGameImage(item.AppID)}
+          style={{ width: 100, height: 100 }}
+        />
+      ))}
+    </ScrollView>
+  );
+}
