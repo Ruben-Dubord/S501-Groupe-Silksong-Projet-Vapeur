f4e6038f29fb11a02aadc422234aaf1a4e0d0736
Yanis <yanis.naitmakhlouf@gmail.com>
2026-01-20 14:09:41 +0100

    commeentaire sur le fichier storage.ts

--- scripts/storage.ts
+++ scripts/storage.ts
@@ -1,9 +1,16 @@
 // scripts/storage.ts
+// Ce fichier gère le stockage local des préférences utilisateur et de l'historique des jeux.
+// Il utilise AsyncStorage pour persister les données dans l'application React Native.
+
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const USER_PREFERENCES_KEY = 'userPreferences';
 const USER_HISTORY_KEY = 'userHistory';
 
+/**
+ * Récupère les préférences utilisateur depuis le stockage local.
+ * @returns Un objet contenant les préférences utilisateur ou un objet vide en cas d'erreur.
+ */
 export const getUserPreferences = async (): Promise<Record<string, number>> => {
   try {
     const preferences = await AsyncStorage.getItem(USER_PREFERENCES_KEY);
@@ -14,6 +21,10 @@ export const getUserPreferences = async (): Promise<Record<string, number>> => {
   }
 };
 
+/**
+ * Met à jour les préférences utilisateur dans le stockage local.
+ * @param newPreferences Les nouvelles préférences à stocker.
+ */
 export const updateUserPreferences = async (newPreferences: Record<string, number>): Promise<void> => {
   try {
     const preferencesString = JSON.stringify(newPreferences);
@@ -23,6 +34,10 @@ export const updateUserPreferences = async (newPreferences: Record<string, numbe
   }
 };
 
+/**
+ * Récupère l'historique des actions utilisateur depuis le stockage local.
+ * @returns Un tableau d'objets représentant l'historique ou un tableau vide en cas d'erreur.
+ */
 export const getUserHistory = async (): Promise<Array<{ gameId: number; liked: boolean; timestamp: string }>> => {
   try {
     const userHistory = await AsyncStorage.getItem(USER_HISTORY_KEY);
@@ -33,6 +48,11 @@ export const getUserHistory = async (): Promise<Array<{ gameId: number; liked: b
   }
 };
 
+/**
+ * Ajoute une nouvelle entrée à l'historique utilisateur.
+ * @param gameId L'identifiant du jeu.
+ * @param liked Indique si le jeu a été aimé ou non.
+ */
 export const addToHistory = async (gameId: number, liked: boolean): Promise<void> => {
   try {
     const history = await AsyncStorage.getItem(USER_HISTORY_KEY);
